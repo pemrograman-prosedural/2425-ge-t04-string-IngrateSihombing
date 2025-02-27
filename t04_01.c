fac0ba3297049f478745ac6140daed43b9364d09
IngrateSihombing <ingratejoy@gmail.com>
2025-02-27 14:08:19 +0700

    PAIR 11

--- t04_01.c
+++ t04_01.c
@@ -1,10 +1,24 @@
-// NIM - Name
-// NIM - Name
+// 12S24014 - Arion Manurung
+// 12S24022 - Ingrate Joy Sihombing
 
 #include <stdio.h>
 
 int main(int _argv, char **_argc) {
-  // codes
+ void UbahMenjadiTeks (int nilai_ascii) {
+printf ("%c" , nilai_ascii) ;
+  
+ }
+  int i ;
+  char str [60] ;
+  scanf ("%60[^\n]" , str);
 
+  for(i = 0; str[i]!='\0' && str[i+1]!='\0' && str[i+2]!='\0' ; i+=3) {
+    int nilai_ascii = 0;
+    sscanf(&str[i], "%3d", &nilai_ascii);
+    UbahMenjadiTeks(nilai_ascii) ;
+
+  }
+
+  printf("\n") ;
   return 0;
 }
--- t04_02.c
+++ t04_02.c
@@ -1,10 +1,17 @@
-// NIM - Name
-// NIM - Name
+// 12S24014 - Arion Manurung
+// 12S24022 - Ingrate Joy Sihombing
 
 #include <stdio.h>
 
 int main(int _argv, char **_argc) {
-  // codes
+  char str[20] ;
+  scanf("%20[^\n]" , str) ;
+
+  for(int i = 0; str[i]!= '\0' ; i++) {
+    printf("%03d" , (int)str[i]) ;
+  
+  }
+printf ("\n") ;
 
   return 0;
 }
